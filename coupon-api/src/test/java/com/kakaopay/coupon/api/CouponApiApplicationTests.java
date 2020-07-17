49b2b619f9b7d8475f7da099b4886f84d4fe11fb
YoungWukJeon <kdh10080@gmail.com>
2020-07-18 02:56:47 +0900

    Coupon API Initial Commit

---
+++ coupon-api/src/test/java/com/kakaopay/coupon/api/CouponApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kakaopay.coupon.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
