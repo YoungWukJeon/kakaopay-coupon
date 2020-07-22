9b41c3698efe03cd097a489b1824205c7eea9465
YoungWukJeon <kdh10080@gmail.com>
2020-07-23 08:40:09 +0900

    Coupon Expiration Notification Load 프로젝트 구조 설계

---
+++ coupon-expiration-notification-load/src/main/java/com/kakaopay/coupon/expiration/notification/load/CouponExpirationNotificationLoadApplication.java
@@ -0,0 +1,7 @@
+package com.kakaopay.coupon.expiration.notification.load;
+
+public class CouponExpirationNotificationLoadApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
