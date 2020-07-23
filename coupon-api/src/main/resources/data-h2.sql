c28655b1cd4def6f1f32de4f040e5cc2108eb6b0
YoungWukJeon <kdh10080@gmail.com>
2020-07-24 05:44:14 +0900

    local용 h2에 기본 유저(USER) 권한을 가진 user 1명 추가(data.sql)

--- coupon-api/src/main/resources/data-h2.sql
+++ coupon-api/src/main/resources/data-h2.sql
@@ -1,3 +1,5 @@
+INSERT INTO user VALUES (1, 'test', '$2a$10$HHwHOa6BEwS0HHVuugOrdupQv40bSExI6WpJY/XMw6blp/ki/NALu');
 INSERT INTO user VALUES (2, 'admin', '$2a$10$HHwHOa6BEwS0HHVuugOrdupQv40bSExI6WpJY/XMw6blp/ki/NALu');
 
+INSERT INTO user_entity_roles VALUES (1, 'ROLE_USER');
 INSERT INTO user_entity_roles VALUES (2, 'ROLE_USER'), (2, 'ROLE_ADMIN');
